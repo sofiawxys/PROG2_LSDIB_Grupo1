b09cb344f306b731c5a3cd98540f043915d8b63b
dianalopes2710 <up202508276@up.pt>
2026-04-09 12:03:24 +0100

    classe enfermaria

--- codigo_grupo1/src/Enfermaria.java
+++ codigo_grupo1/src/Enfermaria.java
@@ -1,5 +1,41 @@
 public abstract class Enfermaria {
 
     private int idEnfermaria;
+    private int numCamas;
+    private int episodios;
+
+    public Enfermaria(int idEnfermaria, int numCamas, int episodios) {
+        this.idEnfermaria = idEnfermaria;
+        this.numCamas = numCamas;
+        this.episodios = episodios;
+    }
+
+    public int getIdEnfermaria() {
+        return idEnfermaria;
+    }
+
+    public void stIdEnfermaria(int idEnfermaria) {
+        this.idEnfermaria = idEnfermaria;
+    }
+
+    public int getNumCamas (){
+        return numCamas;
+    }
+
+    public void setNumCamas(int numCamas){
+        this.numCamas = idEnfermaria;
+    }
+
+    public int getEpisodios (){
+        return episodios;
+    }
+
+    public void setEpisodios(int idEnfermaria){
+        this.episodios = episodios;
+    }
+
+
+
 
 }
+
